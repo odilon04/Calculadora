01311d9ffaf9aa4c18b5f21cb3bfd2ae8159b22d
Rhenan <rhenan_konrad@hotmail.com>
2019-04-15 20:31:54 -0300

    edit7

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
-function calcular() {
-    var idadeElement = window.document.getElementById("idade");
-    var idadeString = idadeElement.value;
+function calcularQuantidade() {
+    var quantidade_de_carne = window.document.getElementById("adultos1");
+    var adultos1String = adultos1Element.value;
+    var quantidade_de_carne = window.document.getElementById("adultos2");
+    var adultos2String = adultos2Element.value;
+    var quantidade_de_carne = window.document.getElementById("criancas");
+    var criancasString = criancasElement.value;
+    
     console.log("idadeString = " + idadeString);
     var idade = parseInt(idadeString);
     if (idade >= 18) {
