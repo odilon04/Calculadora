8633dc923a6ef11c0400c2f027581f6a487708df
Rhenan <rhenan_konrad@hotmail.com>
2019-04-15 19:46:27 -0300

    Edit2

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function calcular() {
+    var idadeElement = window.document.getElementById("idade");
+    var idadeString = idadeElement.value;
+    console.log("idadeString = " + idadeString);
+    var idade = parseInt(idadeString);
+    if (idade >= 18) {
+        alert("MAIOR de idade.");
+    } else {
+        alert("MENOR de idade.");
+    }
+}
