f78ca7a07e5150bd27695b69cfff12a6882e4957
Rhenan <rhenan_konrad@hotmail.com>
2019-04-15 21:28:56 -0300

    edit15

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function calcular() {
     var adultos2 = parseInt(document.querySelector('#adultos2').value);
     var criancas = parseInr(document.querySelector('#criancas').value);
 
-    var calcularCarne = (adultos1 * 0,5) + (criancas * 0,25);
+    var calcularCarne = (adultos1 * 0.5) + (criancas * 0.25);
     var calcularPao = adultos2 * 5;
 
-    alert("É necessário" +calcularCarne+ "Kg de carne");
-    alert("É necessário" +calcularPao+ "Pães");
+    //alert("É necessário" +calcularCarne+ "Kg de carne");
+    //alert("É necessário" +calcularPao+ "Pães");
+
+    alert(calcularCarne);
     
 }
